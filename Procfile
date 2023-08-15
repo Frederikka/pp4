7512d9fe818f32ab4fb8ea7cfd786e32a9bdcf82
Freda Toal <Freda.toal.work@gmail.com>
2023-08-15 10:29:24 +0000

    Deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn scranshaped.wsgi
---
+++ scrannin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScranninConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scrannin'
